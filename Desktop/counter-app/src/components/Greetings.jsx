1092aaa080cfd66a8cdd268f24a6119e6121bf14
GanapathiMangipudi <57028826+GanapathiMangipudi@users.noreply.github.com>
2025-09-28 23:24:52 +0530

    Delete Desktop/counter-app directory

--- Desktop/counter-app/src/components/Greetings.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Greeting({ name }) {
-  return (
-    <div className="alert alert-primary" role="alert">
-      Hello, {name ? name : "Guest"}!
-    </div>
-  );
-}
